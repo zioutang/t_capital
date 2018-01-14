0ec25d5641a13a5bf0bb290dc4dd57b6d7bfa38c
zioutang <tangziou@gmail.com>
2018-01-13 22:26:36 -0800

    search

--- reactApp/Components/Grid.js
+++ reactApp/Components/Grid.js
@@ -2,28 +2,27 @@ import {
   GridList,
   GridTile
 } from 'material-ui/GridList';
+import FlatButton from 'material-ui/FlatButton';
 import MenuItem from 'material-ui/MenuItem';
-// import {
-//   DropDownMenuOpenImmediateExample
-// } from './DropDown.js';
-import DropDownMenu from 'material-ui/DropDownMenu';
-
+import TextField from 'material-ui/TextField';
 import React from 'react';
 import IconButton from 'material-ui/IconButton';
 import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
-// import SearchBar from 'material-ui-search-bar'
+import RaisedButton from 'material-ui/RaisedButton';
+import Drawer from 'material-ui/Drawer';
 
 const styles = {
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
+    // display: 'flex',
+    // flexWrap: 'wrap',
+    // justifyContent: 'space-around',
   },
   gridList: {
-    width: 900,
-    height: 1200,
-    overflowY: 'auto',
+    // width: 900,
+    // height: 1200,
+    // display: 'flex',
+    // overflowY: 'auto',
   },
 };
 
@@ -31,21 +30,21 @@ const tilesData = [{
     img: 'https://imgur.com/ovNlwDV.jpg',
     title: 'Jay1',
     author: 'jill111',
-    level: 1,
+    level: 3,
     time: 1,
   },
   {
     img: 'https://imgur.com/KnqSIuu.jpg',
     title: 'Jay2',
     author: 'pashminu',
-    level: 2,
+    level: 3,
     time: 2,
   },
   {
     img: 'https://imgur.com/Z3l7Uvj.jpg',
     title: 'Jay3',
     author: 'Danson67',
-    level: 3,
+    level: 2,
     time: 3,
   },
   {
@@ -93,45 +92,91 @@ export default class GridListExampleSimple extends React.Component {
     super(props);
     this.state = {
       value: 1,
-      data: tilesData
+      data: tilesData,
+      open: false,
+      label: 'Newest'
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
-  handleChange(event, index, value) {
+  handleToggle() {
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+  handleChange(value, event) {
     let newList = [];
+    let label = '';
     if (value === 1) {
       this.state.data.sort((a, b) => {
         return a.time - b.time;
       })
       newList = this.state.data;
+      label = 'Newest';
     }
     if (value === 2) {
       this.state.data.sort((a, b) => {
         return a.level - b.level;
       })
       newList = this.state.data;
+      label = 'Easiest';
     }
     if (value === 3) {
       this.state.data.sort((a, b) => {
         return b.level - a.level;
       })
       newList = this.state.data;
+      label = 'Hardiest';
     }
-    console.log(newList);
     this.setState({
       value: value,
-      data: newList
+      data: newList,
+      open: false,
+      label: label
+    });
+  }
+  handleSearch(e) {
+    e.preventDefault();
+    let filter = tilesData.filter(item => {
+      return item.title.toLowerCase().indexOf(e.target.value.toLowerCase()) !== -1;
+    })
+    this.setState({
+      data: filter
     });
   }
   render() {
     return (
       <div style={styles.root}>
         <div>
-          <DropDownMenu value={this.state.value} onChange={this.handleChange} openImmediately={false}>
-            <MenuItem value={1} primaryText="Newest" />
-            <MenuItem value={2} primaryText="Easiest" />
-            <MenuItem value={3} primaryText="Hardiest" />
-          </DropDownMenu>
+          <Drawer
+            openSecondary={true}
+            docked={false}
+            width={'100%'}
+            open={this.state.open}
+            onRequestChange={(open) => this.setState({open})}
+            disableSwipeToOpen={true}
+          >
+            <FlatButton
+              label="Back"
+              onClick={this.handleToggle}
+            />
+            <MenuItem onClick={this.handleChange.bind(this, 1)}>Newest</MenuItem>
+            <MenuItem onClick={this.handleChange.bind(this, 2)}>Easiest</MenuItem>
+            <MenuItem onClick={this.handleChange.bind(this, 3)}>Hardiest</MenuItem>
+          </Drawer>
+          <div >
+            <TextField
+              floatingLabelText="Search"
+              onChange={this.handleSearch.bind(this)}
+            />
+            <span>   Sort by
+            <FlatButton
+              label={this.state.label}
+              onClick={this.handleToggle}
+            />
+            </span>
+          </div>
           <GridList
             cellHeight={400}
             style={styles.gridList}
@@ -145,8 +190,7 @@ export default class GridListExampleSimple extends React.Component {
                   <div>
                   <span>by <b>{tile.author}</b></span>
                   <div>Level: <b>{tile.level}</b></div>
-                  </div>}
-              >
+                  </div>}>
                 <img src={tile.img} />
               </GridTile>
             ))}
@@ -157,26 +201,6 @@ export default class GridListExampleSimple extends React.Component {
     )
   }
 }
-// const GridListExampleSimple = () => (
-//   <div style={styles.root}>
-//     <GridList
-//       cellHeight={180}
-//       style={styles.gridList}
-//     >
-//       <Subheader>Jay's music</Subheader>
-//       {tilesData.map((tile) => (
-//         <GridTile
-//           key={tile.img}
-//           title={tile.title}
-//           subtitle={<span>by <b>{tile.author}</b></span>}
-//           actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-//         >
-//           <img src={tile.img} />
-//         </GridTile>
-//       ))}
-//     </GridList>
-//   </div>
-// );
 module.exports = {
   GridListExampleSimple
 };
